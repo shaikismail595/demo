4d182fa55d61dbf630190e0a4b8dc504bd82723a
Aadarsh  Raghuvanshi <aadarsh.raghuvanshi@Yash.com>
2024-08-29 12:33:06 +0530

    Adding logger for  all level

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,14 +1,26 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+import com.example.demo.controller.AuthServerDetailsController;
+
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class DemoApplication {
+	private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
+		logger.info("Info logger Message!");
+		logger.warn("Warn logger Message!");
+		logger.error("Error logger Message!");
+		logger.trace("Trace logger Message!");
+		logger.debug("Debug logger Message!");
+
 	}
 
 }
