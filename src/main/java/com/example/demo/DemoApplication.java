4fec95c482d3e71734f92c37025714e17fa7a5ce
Tapan Akolekar <tapan.akolekar@Yash.com>
2024-09-17 13:48:22 +0530

    revert jasypt

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,22 +2,13 @@ package com.example.demo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.PropertySource;
-
-import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
-@EnableEncryptableProperties
-@PropertySource(name="EncryptedProperties", value = "classpath:encrypted.properties")
 public class DemoApplication {
 	private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
-	
-	@Value("${secret.property}")
-	private static String prop;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
@@ -27,8 +18,6 @@ public class DemoApplication {
 		logger.error("Error logger Message!");
 		logger.trace("Trace logger Message!");
 		logger.debug("Debug logger Message!");
-
-		logger.info(prop);
 	}
 
 }
