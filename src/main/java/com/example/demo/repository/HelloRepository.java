d318d6f640ca856bc9eb9537a47cb3a8c472531a
kashutosh2001 <kumarashutosh.01070@gmail.com>
2025-02-11 22:24:00 +0530

    Added HelloRepository.java file which returns String

---
+++ src/main/java/com/example/demo/repository/HelloRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import org.springframework.stereotype.Repository;
+@Repository
+public class HelloRepository {
+    public String hello(){
+        return "HelloYASH.com";
+    }
+}
